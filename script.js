a0ec0ec8345cf0a2891d40b2ed4121d54c531d7e
Vladislav Zagvostkin <vzagvostkin@cic.kz>
2023-06-30 17:25:06 +0600

    2:23:21 Player Sprite - Idle

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ const background = new Sprite({
 		y: 0,
 	},
 	imageSrc: './img/background.png',
-})
+});
+
+const shop = new Sprite({
+	position: {
+		x: 600,
+		y: 128,
+	},
+	imageSrc: './img/shop.png',
+	scale: 2.75,
+	framesMax: 6,
+});
 
 let timerId;
 
@@ -88,6 +98,7 @@ const animate = () => {
 	c.fillStyle = 'black';
 	c.fillRect(0, 0, canvas.width, canvas.height);
 	background.update();
+	shop.update();
 	player.update();
 	enemy.update();
 
